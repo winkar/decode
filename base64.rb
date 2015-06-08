5d2e17e3a6d611b7bb21b984d8fadd199ee8798e
winkar <WinKaR@chenjunyandeMacBook-Pro.local>
2015-06-08 17:45:52 +0800

    输出所有结果, 而不是输出第一个结果后退出

--- base64.rb
+++ base64.rb
@@ -9,23 +9,48 @@ require "base64"
 flick= -> c,x { if x==1 then c.upcase else c.downcase end}
 
 
-target_base64 = ""
+# base64_printable_set = ""
+
+base64_printable_set = []
 
 text.scan(/..../).each do |substr|
+
+  base64_printable_substr = []
+
+  flicked_substr = []
+
   for i in 0..15 do
     # p substr
     s = substr.split("").each_with_index.map do |c, index|
       flick.call(substr[index], (i>>index) & 1)
     end.join('')
+    flicked_substr << s
+  end
 
-    p s
+  flicked_substr = flicked_substr.uniq
+
+  flicked_substr.each do |s|
+    # p s
     bs = Base64.decode64(s)
     if bs.scan(/[[:print:]]/).join == bs then
-      target_base64 << s
-      break
+      # target_base64 << s
+      # break
+      base64_printable_substr << s
     end
   end
-end
-# p target_base64
 
-print Base64.decode64(target_base64)
+  # p base64_printable_substr
+  base64_printable_set << base64_printable_substr
+end
+
+# p base64_printable_set
+
+printable_results = base64_printable_set.inject([""]) do |res_arr, s|
+  res_arr.product(s).collect {|arr| arr.join}
+end
+
+# p printable_results
+
+printable_results.each do |s|
+  p Base64.decode64(s)
+end
